25318ab3fab22ca815775f60988cec9349b95b77
mkhetale <khetalemihir@gmail.com>
2021-03-25 01:21:30 -0400

    gitignore and develop branch

---
+++ WeProvide/Views/Authentication/SignupProvider.swift
@@ -0,0 +1,20 @@
+//
+//  SignupProvider.swift
+//  WeProvide
+//
+//  Created by Mihir Khetale on 3/25/21.
+//
+
+import SwiftUI
+
+struct SignupProvider: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SignupProvider_Previews: PreviewProvider {
+    static var previews: some View {
+        SignupProvider()
+    }
+}
