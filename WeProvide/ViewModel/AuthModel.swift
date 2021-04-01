a7a96b4016e860bfb4fc96e853f3ae47d585158a
mkhetale <khetalemihir@gmail.com>
2021-03-31 20:22:55 -0400

    Storing ImageUrl and user info in Firebase collection

--- WeProvide/ViewModel/AuthModel.swift
+++ WeProvide/ViewModel/AuthModel.swift
@@ -14,11 +14,37 @@ class AuthModel: ObservableObject {
     }
     
     func registerUser(email: String, password: String, fullName: String, profileImage: UIImage) {
-        Auth.auth().createUser(withEmail: email, password: password) { result, error in
+        guard let imageData = profileImage.jpegData(compressionQuality: 0.3) else { return }
+        let filename = NSUUID().uuidString
+        let storageRef = Storage.storage().reference().child(filename)
+        storageRef.putData(imageData, metadata: nil) { _, error in
             if let error = error {
-                print("DEBUG: Error \(error.localizedDescription)")
+                print("Failed to Upload Image \(error.localizedDescription)")
+                return
             }
-            print("DEBUG: User sign up successful")
+            storageRef.downloadURL { url, _ in
+                guard let profileImageUrl = url?.absoluteString else { return }
+                Auth.auth().createUser(withEmail: email, password: password) { result, error in
+                    if let error = error {
+                        print("DEBUG: Error \(error.localizedDescription)")
+                    }
+                    guard let user = result?.user else { return }
+                    let data = ["email": email,
+                                "fullName": fullName,
+                                "profileImageUrl": profileImageUrl,
+                                "userId": user.uid
+                    ]
+                    Firestore.firestore().collection("users").document(user.uid).setData(data) { _ in
+                        print("DEBUG: Successfully uploaded user data")
+                        
+                    }
+                    
+        //            print("DEBUG: User sign up successful")
+                }
+            }
+        
+            
         }
+        
     }
 }
